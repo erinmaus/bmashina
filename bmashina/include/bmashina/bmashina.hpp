8ac2aaf192b07c2c4fba9e55dba0b4b624a9b684
[bk]door.maus <bkdoormaus@protonmail.com>
2017-08-08 08:21:15 -0400

    Added convenience headers.

---
+++ bmashina/include/bmashina/bmashina.hpp
@@ -0,0 +1,20 @@
+// BMASHINA
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2017 [bk]door.maus
+
+#ifndef BMASHINA_BMASHINA_HPP
+#define BMASHINA_BMASHINA_HPP
+
+#include "bmashina/channel.hpp"
+#include "bmashina/composite.hpp"
+#include "bmashina/config.hpp"
+#include "bmashina/decorator.hpp"
+#include "bmashina/node.hpp"
+#include "bmashina/status.hpp"
+#include "bmashina/tree.hpp"
+
+#endif
